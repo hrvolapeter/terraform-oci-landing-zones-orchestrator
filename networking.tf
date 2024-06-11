e8b5cdbea2d40098dff696143f0e5415d9e2b351
Peter Hrvola <peter.hrvola@oracle.com>
2024-06-11 12:44:31 +0200

    Update networking.tf

--- networking.tf
+++ networking.tf
@@ -1,10 +1,10 @@
-# Copyright (c) 2024 Oracle and/or its affiliates.
+i# Copyright (c) 2024 Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
 # Core networking
 module "oci_lz_network" {
   count                   = var.network_configuration != null ? 1 : 0
-  source                  = "git::https://github.com/oracle-quickstart/terraform-oci-cis-landing-zone-networking.git?ref=v0.6.6"
+  source                  = "git::https://github.com/hrvolapeter/terraform-oci-cis-landing-zone-networking.git"
   network_configuration   = var.network_configuration
   compartments_dependency = local.compartments_dependency
   network_dependency      = local.ext_dep_network_map
@@ -14,7 +14,7 @@ module "oci_lz_network" {
 # Network Load Balancers
 module "oci_lz_nlb" {
   count                   = var.nlb_configuration != null ? 1 : 0
-  source                  = "git::https://github.com/hrvolapeter/terraform-oci-cis-landing-zone-networking.git//modules/nlb?ref=v0.6.6"
+  source                  = "git::https://github.com/oracle-quickstart/terraform-oci-cis-landing-zone-networking.git//modules/nlb?ref=v0.6.6"
   nlb_configuration       = var.nlb_configuration
   compartments_dependency = local.compartments_dependency
   network_dependency      = local.network_dependency
